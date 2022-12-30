de00bbe2a0104bf0dcd79948b8c366adeff29190
Valeria <valeria.s@outlook.com.br>
2022-12-30 00:04:12 -0300

    finished project

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -15,8 +15,8 @@ export class Notification{
     private _id: string;
     private props: NotificationProps;
 
-    constructor(props: Replace<NotificationProps, { createdAt?: Date}>){
-        this._id = randomUUID();
+    constructor(props: Replace<NotificationProps, { createdAt?: Date}>, id?: string){
+        this._id = id ?? randomUUID();
         this.props = {
             ...props,
             createdAt: props.createdAt ?? new Date()
@@ -56,8 +56,13 @@ export class Notification{
     public get readAt() : Date | null | undefined{
         return this.props.readAt;
     }
-    public set readAt(readAt : Date | null | undefined) {
-        this.props.readAt = readAt;
+    
+    public read(){
+        this.props.readAt = new Date();
+    }
+
+    public unread(){
+        this.props.readAt = null;
     }
 
     public cancel(){
